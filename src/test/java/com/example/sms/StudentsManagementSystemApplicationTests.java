09a12b3d87961deaaae31b8e7b842805816375c4
sanjananbhaskar <sanjananbhaskar@gmail.com>
2024-10-12 14:00:59 +0530

    commit

---
+++ src/test/java/com/example/sms/StudentsManagementSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
